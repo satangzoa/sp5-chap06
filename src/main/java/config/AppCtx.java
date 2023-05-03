785e41e901bf2a0bd0c06d607162e020146fddb9
A <A@DESKTOP-NKSGQVR>
2023-05-03 18:19:31 +0900

    spring5

---
+++ src/main/java/config/AppCtx.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import spring.Client;
+
+
+@Configuration
+public class AppCtx {
+	@Bean
+	public  Client client() {
+		Client client = new Client();
+		client.setHost("host");
+		return client;
+	}
+}
